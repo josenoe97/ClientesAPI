c758ec5a45e836f55b9f6d62a873376cfa8ac417
Jose Edson Jr <edsonjunior9701@outlook.com>
2025-06-10 05:56:34 -0300

    feat: criacao da dto EnderecoDTO

--- ClientesAPI/Application/DTOs/ClienteDTO.cs
+++ ClientesAPI/Application/DTOs/ClienteDTO.cs
@@ -1,44 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ClientesAPI.Application.DTOs
 {
     public class ClienteDTO
     {
-        public class Clientes
-        {
-            [Key]
-            public Guid Id { get; set; } = Guid.NewGuid();
+        [Key]
+        [JsonIgnore]
+        public Guid Id { get; set; } = Guid.NewGuid();
 
-            [Required(ErrorMessage = "O nome é obrigatório.")]
-            public string Nome { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Nome { get; set; }
 
-            [Required(ErrorMessage = "O e-mail é obrigatório.")]
-            [EmailAddress(ErrorMessage = "E-mail em formato inválido.")]
-            public string Email { get; set; }
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "E-mail em formato inválido.")]
+        public string Email { get; set; }
 
-            public string Telefone { get; set; }
+        public string Telefone { get; set; }
 
-            [Required(ErrorMessage = "O endereço é obrigatório.")]
-            public Endereco Endereco { get; set; }
-        }
+        [Required(ErrorMessage = "O endereço é obrigatório.")]
+        public EnderecoDTO Endereco { get; set; }
 
-        public class Endereco
-        {
-            [Required(ErrorMessage = "A rua é obrigatória.")]
-            public string Rua { get; set; }
-
-            [Required(ErrorMessage = "O número é obrigatório.")]
-            public string Numero { get; set; }
-
-            [Required(ErrorMessage = "A cidade é obrigatória.")]
-            public string Cidade { get; set; }
-
-            [Required(ErrorMessage = "O estado é obrigatório.")]
-            public string Estado { get; set; }
-
-            [Required(ErrorMessage = "O CEP é obrigatório.")]
-            [RegularExpression(@"^\d{5}-?\d{3}$", ErrorMessage = "Formato de CEP inválido.")]
-            public string CEP { get; set; }
-        }
     }
 }
---
+++ ClientesAPI/Application/DTOs/EnderecoDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientesAPI.Application.DTOs
+{
+    public class EnderecoDTO
+    {
+
+        [Required(ErrorMessage = "A rua é obrigatória.")]
+        public string Rua { get; set; }
+
+        [Required(ErrorMessage = "O número é obrigatório.")]
+        public string Numero { get; set; }
+
+        [Required(ErrorMessage = "A cidade é obrigatória.")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O estado é obrigatório.")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "O CEP é obrigatório.")]
+        [RegularExpression(@"^\d{5}-?\d{3}$", ErrorMessage = "Formato de CEP inválido.")]
+        public string CEP { get; set; }
+    }
+}
